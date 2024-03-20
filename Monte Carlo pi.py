1e8168c2675646cf6513c88d612afd69cd2a6efc
Corgi <weirdfrog44@gmail.com>
2024-03-20 17:44:38 +0800

    monte carlo

--- Monte Carlo pi.py
+++ Monte Carlo pi.py
@@ -1,3 +1,4 @@
+'''
 import random
 import math
 inside_circle = 0
@@ -8,3 +9,11 @@ for _ in range(n):
     if math.sqrt((x-0.5)**2 + (y-0.5)**2) < 0.5:
         inside_circle += 1
 print (inside_circle / (n/4))
+'''
+
+import numpy as np
+n = 300000000
+x = np.random.rand(n)
+y = np.random.rand(n)
+inside_circle = np.sum(x**2 + y**2 < 1)
+print(inside_circle / (n / 4))
